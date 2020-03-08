11be375f5ae980ec5a8ee90e7f8644dc60fd5d8c
ElElias <32906696+eserrania@users.noreply.github.com>
2020-03-07 23:01:25 -0600

    Added transitions

--- src/scatterplot.js
+++ src/scatterplot.js
@@ -1,23 +1,105 @@
 import { select } from "d3-selection";
 import { scaleLog, scaleSqrt, scaleOrdinal, scaleLinear } from "d3-scale";
 import { axisLeft, axisBottom } from "d3-axis";
+import { transition } from "d3-transition";
 import d3Tip from "d3-tip";
 
 export default function scatterplot(data) {
-  const state = {
-    xyear: 2018,
-    yyear: 2019,
-    size: "All"
-  };
-
   const years = [2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019];
   const sizes = ["All", "Large", "Medium", "Small"];
 
   const height = 700;
   const width = 900;
   const sizeMax = 30;
-
   const margin = { top: 50, right: 200, bottom: 100, left: 50 };
+
+  const state = {
+    xyear: 2018,
+    yyear: 2019,
+    size: "All"
+  };
+
+  var xLabel =
+    "Homicides per 100k inhabitants (" + state.xyear.toString() + ")";
+  var yLabel =
+    "Homicides per 100k inhabitants (" + state.yyear.toString() + ")";
+  const sizeLabel = "Population(" + state.yyear.toString() + ")";
+  var yhom = "homrate" + state.yyear.toString();
+  var xhom = "homrate" + state.xyear.toString();
+
+  if (state.yyear < 2015) {
+    var ypop = "pop2010";
+  } else {
+    var ypop = "pop" + state.yyear.toString();
+  }
+
+  function filterData() {
+    var newData = data.filter(function(d) {
+      if (state.size === "Large") {
+        return d[ypop] >= 100000 && d[yhom] > 1 && d[xhom] > 1;
+      } else if (state.size === "Medium") {
+        return (
+          d[ypop] >= 25000 && d[ypop] < 100000 && d[yhom] > 1 && d[xhom] > 1
+        );
+      } else if (state.size === "Small") {
+        return d[ypop] < 25000 && d[yhom] > 1 && d[xhom] > 1;
+      } else {
+        return d[yhom] > 1 && d[xhom] > 1;
+      }
+    });
+    return newData;
+  }
+
+  function update() {
+    xLabel = "Homicides per 100k inhabitants (" + state.xyear.toString() + ")";
+    yLabel = "Homicides per 100k inhabitants (" + state.yyear.toString() + ")";
+    yhom = "homrate" + state.yyear.toString();
+    xhom = "homrate" + state.xyear.toString();
+    if (state.yyear < 2015) {
+      var ypop = "pop2010";
+    } else {
+      var ypop = "pop" + state.yyear.toString();
+    }
+  }
+
+  function updateTip() {
+    var tip = d3Tip()
+      .attr("class", "d3-tip")
+      .offset([-10, 0])
+      .html(function(d) {
+        return (
+          "<strong>Municipality:</strong> <span style='color:black'>" +
+          d.municipality +
+          "</br>" +
+          "<strong>State:</strong> <span style='color:black'>" +
+          d.state +
+          "</span>" +
+          "</br>" +
+          "<strong>" +
+          state.xyear.toString() +
+          " homicide rate:" +
+          "</strong> <span style='color:black'>" +
+          Number(d[xhom]).toFixed(2) +
+          "</span>" +
+          "</br>" +
+          "<strong>" +
+          state.yyear.toString() +
+          " homicide rate:" +
+          "</strong> <span style='color:black'>" +
+          Number(d[yhom]).toFixed(2) +
+          "</span>" +
+          "</br>" +
+          "<strong>" +
+          state.yyear.toString() +
+          " population:" +
+          "</strong> <span style='color:black'>" +
+          d[ypop] +
+          "</span>"
+        );
+      });
+  }
+
+  var filteredData = filterData();
 
   const dropDownTextX = select(".drop-down")
     .append("text")
@@ -30,7 +112,32 @@ export default function scatterplot(data) {
     .attr("class", "menu")
     .on("change", function dropdownReaction(d) {
       state.xyear = this.value;
-      render();
+      update();
+      var filteredData = filterData();
+
+      var xLabels = svg.selectAll(".xlabel").data([xLabel]);
+      xLabels
+        .enter()
+        .append("text")
+        .attr("class", "xlabel")
+        .style("text-anchor", "middle")
+        .merge(xLabels)
+        .attr("transform", `translate(${width / 2}, ${height - 30})`)
+        .text(d => {
+          return d;
+        });
+      updateTip();
+
+      g.selectAll("circle")
+        .data(filteredData)
+        .transition()
+        .duration(1000)
+        .attr("cx", d => xScale(xValue(d)))
+        .attr("cy", d => yScale(yValue(d)))
+        .attr("r", d => sizeScale(sizeValue(d)))
+        .attr("fill", d => colorScale(d.region))
+        .on("mouseover", tip.show)
+        .on("mouseout", tip.hide);
     });
 
   dropDownX
@@ -55,7 +162,36 @@ export default function scatterplot(data) {
     .attr("class", "menu")
     .on("change", function dropdownReaction(d) {
       state.yyear = this.value;
-      render();
+      update();
+      var filteredData = filterData();
+
+      var yLabels = svg.selectAll(".ylabel").data([yLabel]);
+      yLabels
+        .enter()
+        .append("text")
+        .attr("class", "xlabel")
+        .style("text-anchor", "middle")
+        .merge(yLabels)
+        .attr(
+          "transform",
+          `translate(${margin.left - 10}, ${innerHeight / 2 +
+            margin.top}) rotate(270)`
+        )
+        .text(d => {
+          return d;
+        });
+      updateTip();
+
+      g.selectAll("circle")
+        .data(filteredData)
+        .transition()
+        .duration(1000)
+        .attr("cx", d => xScale(xValue(d)))
+        .attr("cy", d => yScale(yValue(d)))
+        .attr("r", d => sizeScale(sizeValue(d)))
+        .attr("fill", d => colorScale(d.region))
+        .on("mouseover", tip.show)
+        .on("mouseout", tip.hide);
     });
 
   dropDownY
@@ -80,7 +216,27 @@ export default function scatterplot(data) {
     .attr("class", "menu")
     .on("change", function dropdownReaction(d) {
       state.size = this.value;
-      render();
+
+      var filteredData = filterData();
+      updateTip();
+
+      var circles = g.selectAll("circle").data(filteredData);
+      circles.exit().remove();
+
+      circles
+        .enter()
+        .append("circle")
+        .attr("r", 5)
+        .merge(circles)
+        .attr("class", "circle")
+        .attr("cx", d => xScale(xValue(d)))
+        .attr("cy", d => yScale(yValue(d)))
+        .attr("r", d => sizeScale(sizeValue(d)))
+        .attr("fill", d => colorScale(d.region))
+        .attr("opacity", 0.5)
+        .attr("transform", `translate(${margin.left}, 0)`)
+        .on("mouseover", tip.show)
+        .on("mouseout", tip.hide);
     });
 
   dropDownSize
@@ -132,222 +288,189 @@ export default function scatterplot(data) {
     .attr("class", "axis")
     .attr("transform", `translate(${margin.left}, 0)`);
 
-  function render() {
-    const xLabel =
-      "Homicides per 100k inhabitants (" + state.xyear.toString() + ")";
-    const yLabel =
-      "Homicides per 100k inhabitants (" + state.yyear.toString() + ")";
-    const sizeLabel = "Population(" + state.yyear.toString() + ")";
-    const yhom = "homrate" + state.yyear.toString();
-    const xhom = "homrate" + state.xyear.toString();
+  const xValue = d => d[xhom];
+  const yValue = d => d[yhom];
+  var sizeValue = d => d[ypop];
 
-    if (state.yyear < 2015) {
-      var ypop = "pop2010";
-    } else {
-      var ypop = "pop" + state.yyear.toString();
-    }
+  xScale.domain([1, 256]).nice();
+  yScale.domain([1, 256]).nice();
+  sizeScale.domain([0, 1848954]);
 
-    var filteredData = data.filter(function(d) {
-      if (state.size === "Large") {
-        return d[ypop] >= 100000 && d[yhom] > 1 && d[xhom] > 1;
-      } else if (state.size === "Medium") {
-        return (
-          d[ypop] >= 25000 && d[ypop] < 100000 && d[yhom] > 1 && d[xhom] > 1
-        );
-      } else if (state.size === "Small") {
-        return d[ypop] < 25000 && d[yhom] > 1 && d[xhom] > 1;
-      } else {
-        return d[yhom] > 1 && d[xhom] > 1;
-      }
+  xAxisG.call(xAxis);
+  yAxisG.call(yAxis);
+
+  g.append("line")
+    .attr("x1", xScale(1))
+    .attr("y1", yScale(1))
+    .attr("x2", xScale(256))
+    .attr("y2", yScale(256))
+    .attr("stroke-width", 2.5)
+    .attr("stroke", "black")
+    .attr("stroke-dasharray", "5,5")
+    .attr("transform", `translate(${margin.left}, 0)`);
+
+  var tip = d3Tip()
+    .attr("class", "d3-tip")
+    .offset([-10, 0])
+    .html(function(d) {
+      return (
+        "<strong>Municipality:</strong> <span style='color:black'>" +
+        d.municipality +
+        "</br>" +
+        "<strong>State:</strong> <span style='color:black'>" +
+        d.state +
+        "</span>" +
+        "</br>" +
+        "<strong>" +
+        state.xyear.toString() +
+        " homicide rate:" +
+        "</strong> <span style='color:black'>" +
+        Number(d[xhom]).toFixed(2) +
+        "</span>" +
+        "</br>" +
+        "<strong>" +
+        state.yyear.toString() +
+        " homicide rate:" +
+        "</strong> <span style='color:black'>" +
+        Number(d[yhom]).toFixed(2) +
+        "</span>" +
+        "</br>" +
+        "<strong>" +
+        state.yyear.toString() +
+        " population:" +
+        "</strong> <span style='color:black'>" +
+        d[ypop] +
+        "</span>"
+      );
     });
 
-    const xValue = d => d[xhom];
-    const yValue = d => d[yhom];
-    const sizeValue = d => d[ypop];
+  var circles = g.selectAll("circle").data(filteredData);
+  circles.exit().remove();
 
-    xScale.domain([1, 256]).nice();
-    yScale.domain([1, 256]).nice();
-    sizeScale.domain([0, 1848954]);
+  circles
+    .enter()
+    .append("circle")
+    .attr("r", 5)
+    .merge(circles)
+    .attr("class", "circle")
+    .attr("cx", d => xScale(xValue(d)))
+    .attr("cy", d => yScale(yValue(d)))
+    .attr("r", d => sizeScale(sizeValue(d)))
+    .attr("fill", d => colorScale(d.region))
+    .attr("opacity", 0.5)
+    .attr("transform", `translate(${margin.left}, 0)`)
+    .on("mouseover", tip.show)
+    .on("mouseout", tip.hide);
 
-    xAxisG.call(xAxis);
-    yAxisG.call(yAxis);
+  g.call(tip);
 
-    g.append("line")
-      .attr("x1", xScale(1))
-      .attr("y1", yScale(1))
-      .attr("x2", xScale(256))
-      .attr("y2", yScale(256))
-      .attr("stroke-width", 2.5)
-      .attr("stroke", "black")
-      .attr("stroke-dasharray", "5,5")
-      .attr("transform", `translate(${margin.left}, 0)`);
+  var xLabels = svg.selectAll(".xlabel").data([xLabel]);
 
-    const circles = g.selectAll("circle").data(filteredData);
-    circles.exit().remove();
-    // add d3 transtions here
+  xLabels
+    .enter()
+    .append("text")
+    .attr("class", "xlabel")
+    .style("text-anchor", "middle")
+    .merge(xLabels)
+    .attr("transform", `translate(${width / 2}, ${height - 30})`)
+    .text(d => {
+      return d;
+    });
 
-    const tip = d3Tip()
-      .attr("class", "d3-tip")
-      .offset([-10, 0])
-      .html(function(d) {
-        return (
-          "<strong>Municipality:</strong> <span style='color:black'>" +
-          d.municipality +
-          "</br>" +
-          "<strong>State:</strong> <span style='color:black'>" +
-          d.state +
-          "</span>" +
-          "</br>" +
-          "<strong>" +
-          state.xyear.toString() +
-          " homicide rate:" +
-          "</strong> <span style='color:black'>" +
-          Number(d[xhom]).toFixed(2) +
-          "</span>" +
-          "</br>" +
-          "<strong>" +
-          state.yyear.toString() +
-          " homicide rate:" +
-          "</strong> <span style='color:black'>" +
-          Number(d[yhom]).toFixed(2) +
-          "</span>" +
-          "</br>" +
-          "<strong>" +
-          state.yyear.toString() +
-          " population:" +
-          "</strong> <span style='color:black'>" +
-          d[ypop] +
-          "</span>"
-        );
-      });
+  var yLabels = svg.selectAll(".ylabel").data([yLabel]);
 
-    circles
-      .enter()
-      .append("circle")
-      .attr("r", 5)
-      .merge(circles)
-      .attr("class", "circle")
-      .attr("cx", d => xScale(xValue(d)))
-      .attr("cy", d => yScale(yValue(d)))
-      .attr("r", d => sizeScale(sizeValue(d)))
-      .attr("fill", d => colorScale(d.region))
-      .attr("opacity", 0.5)
-      .attr("transform", `translate(${margin.left}, 0)`)
-      .on("mouseover", tip.show)
-      .on("mouseout", tip.hide);
+  yLabels
+    .enter()
+    .append("text")
+    .attr("class", "ylabel")
+    .style("text-anchor", "middle")
+    .merge(yLabels)
+    .attr(
+      "transform",
+      `translate(${margin.left - 10}, ${innerHeight / 2 +
+        margin.top}) rotate(270)`
+    )
+    .text(d => {
+      return d;
+    });
 
-    g.call(tip);
+  const legendHeight = 120;
 
-    const xLabels = svg.selectAll(".xlabel").data([xLabel]);
+  const legendY = scaleLinear()
+    .domain([0, colorDomain.length])
+    .range([0, legendHeight]);
 
-    xLabels
-      .enter()
-      .append("text")
-      .attr("class", "xlabel")
-      .style("text-anchor", "middle")
-      .merge(xLabels)
-      .attr("transform", `translate(${width / 2}, ${height - 30})`)
-      .text(d => {
-        return d;
-      });
+  const legend = svg
+    .append("g")
+    .attr("class", "legend")
+    .attr("transform", `translate(${innerWidth + 130}, ${margin.top})`);
 
-    const yLabels = svg.selectAll(".ylabel").data([yLabel]);
+  legend
+    .append("text")
+    .attr("class", "legend-title")
+    .attr("transform", `translate(0, 50)`)
+    .text("Region");
 
-    yLabels
-      .enter()
-      .append("text")
-      .attr("class", "ylabel")
-      .style("text-anchor", "middle")
-      .merge(yLabels)
-      .attr(
-        "transform",
-        `translate(${margin.left - 10}, ${innerHeight / 2 +
-          margin.top}) rotate(270)`
-      )
-      .text(d => {
-        return d;
-      });
+  // add the rects
+  legend
+    .selectAll(".legend-circle")
+    .data(colorDomain)
+    .enter()
+    .append("circle")
+    .attr("class", "legend-circle")
+    .attr("r", d => 10)
+    .attr("cx", 0)
+    .attr("cy", (_, i) => legendY(i) + 72)
+    .attr("fill", d => colorScale(d));
 
-    const legendHeight = 120;
+  legend
+    .selectAll(".legend-text")
+    .data(colorDomain)
+    .enter()
+    .append("text")
+    .attr("class", "legend-text")
+    .attr("x", 20)
+    .attr("y", (_, i) => legendY(i) + 72)
+    .attr("fill", "black")
+    .attr("alignment-baseline", "central")
+    .text(d => d);
 
-    const legendY = scaleLinear()
-      .domain([0, colorDomain.length])
-      .range([0, legendHeight]);
+  const legendS = svg
+    .append("g")
+    .attr("class", "legendS")
+    .attr("transform", `translate(${innerWidth + 130}, ${margin.top + 150})`);
 
-    const legend = svg
-      .append("g")
-      .attr("class", "legend")
-      .attr("transform", `translate(${innerWidth + 130}, ${margin.top})`);
+  legendS
+    .append("text")
+    .attr("class", "legend-title")
+    .attr("transform", `translate(0, 100)`)
+    .text("Population");
 
-    legend
-      .append("text")
-      .attr("class", "legend-title")
-      .attr("transform", `translate(0, 50)`)
-      .text("Region");
+  const legendYS = scaleLinear()
+    .domain([0, 1, 2])
+    .range([0, 40]);
 
-    // add the rects
-    legend
-      .selectAll(".legend-circle")
-      .data(colorDomain)
-      .enter()
-      .append("circle")
-      .attr("class", "legend-circle")
-      .attr("r", d => 10)
-      .attr("cx", 0)
-      .attr("cy", (_, i) => legendY(i) + 72)
-      .attr("fill", d => colorScale(d));
+  legendS
+    .selectAll(".legend-circle")
+    .data([10000, 100000, 1000000])
+    .enter()
+    .append("circle")
+    .attr("class", "legend-circle")
+    .attr("r", d => sizeScale(d))
+    .attr("cx", 0)
+    .attr("cy", (_, i) => legendYS(i) + 120)
+    .attr("fill", "black");
 
-    legend
-      .selectAll(".legend-text")
-      .data(colorDomain)
-      .enter()
-      .append("text")
-      .attr("class", "legend-text")
-      .attr("x", 20)
-      .attr("y", (_, i) => legendY(i) + 72)
-      .attr("fill", "black")
-      .attr("alignment-baseline", "central")
-      .text(d => d);
-
-    const legendS = svg
-      .append("g")
-      .attr("class", "legendS")
-      .attr("transform", `translate(${innerWidth + 130}, ${margin.top + 150})`);
-
-    legendS
-      .append("text")
-      .attr("class", "legend-title")
-      .attr("transform", `translate(0, 100)`)
-      .text("Population");
-
-    const legendYS = scaleLinear()
-      .domain([0, 1, 2])
-      .range([0, 40]);
-
-    legendS
-      .selectAll(".legend-circle")
-      .data([10000, 100000, 1000000])
-      .enter()
-      .append("circle")
-      .attr("class", "legend-circle")
-      .attr("r", d => sizeScale(d))
-      .attr("cx", 0)
-      .attr("cy", (_, i) => legendYS(i) + 120)
-      .attr("fill", "black");
-
-    legendS
-      .selectAll(".legendS-text")
-      .data(["10,000", "100,000", "1,000,000"])
-      .enter()
-      .append("text")
-      .attr("class", "legend-text")
-      .attr("x", 35)
-      .attr("y", (_, i) => legendYS(i) + 128)
-      .attr("fill", "black")
-      .text(d => d);
-  }
-
-  render();
+  legendS
+    .selectAll(".legendS-text")
+    .data(["10,000", "100,000", "1,000,000"])
+    .enter()
+    .append("text")
+    .attr("class", "legend-text")
+    .attr("x", 35)
+    .attr("y", (_, i) => legendYS(i) + 128)
+    .attr("fill", "black")
+    .text(d => d);
 }
 
 // Sources:
